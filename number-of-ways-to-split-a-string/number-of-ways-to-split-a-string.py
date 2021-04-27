6ba4d6260c4be6dbd4ae4e69b033d733c8aa2886
Abdujabbar Mirkhalikov <abdujabbar.mirkhalikov@vendors.cloverhealth.com>
2021-04-28 00:05:42 +0500

    Time: 284 ms (11.46%), Space: 14.9 MB (93.40%) - LeetHub

--- number-of-ways-to-split-a-string/number-of-ways-to-split-a-string.py
+++ number-of-ways-to-split-a-string/number-of-ways-to-split-a-string.py
@@ -2,19 +2,27 @@ m = 10**9 + 7
 
 class Solution:
     def numWays(self, s: str) -> int:
-        pf = list(accumulate(int(c) for c in s))
-        cnt = Counter(pf)
-        n  = pf[-1]
+        total = s.count('1')
         
-        if n % 3:
+        n = len(s)
+        
+        if total % 3 != 0:
             return 0
-    
-        if n == 0:
-            return ((len(s)-1)*(len(s)-2)//2)%m
-
-        c = 0
-        for p in pf[:-1]:
-            if p*2 == n-p:
-                c += cnt[n-p]
-
-        return c%m
+        
+        if total == 0:
+            return (((n - 1) * (n - 2)) // 2) % m
+        
+        f1 = 0
+        f2 = 0
+        
+        t = 0
+        for c in s:
+            t += int(c)
+            if t == (total // 3):
+                f1 += 1
+            
+            if t == (total // 3) * 2:
+                f2 += 1
+        
+        
+        return (f1 * f2) % m
