a391f72f85145d96ac9824520e5714e2ace616ab
Abdujabbar Mirkhalikov <abdujabbar.mirkhalikov@vendors.cloverhealth.com>
2021-06-30 16:55:38 +0500

    Time: 52 ms (66.67%), Space: 14.6 MB (20.23%) - LeetHub

--- maximum-sum-of-two-non-overlapping-subarrays/maximum-sum-of-two-non-overlapping-subarrays.py
+++ maximum-sum-of-two-non-overlapping-subarrays/maximum-sum-of-two-non-overlapping-subarrays.py
@@ -1,24 +1,16 @@
 class Solution:
-    def trail(self, A, a, b):
-        # We want to find the largest value of sa, 
-        # and then the corresponding sb that gives largest result
-        
-        sa = ba = sum(A[:a]) # sum from beggining
-        sb = sum(A[a:b+a])   # sum right after "sa"
-        
-        best = ba+sb
-        
-        for i in range(b+a,len(A)):
-            sa += A[i-b]- A[i-b-a] # Sum of sa
-            sb += A[i]  - A[i-b] # Sum of sb
-            
-            ba = max( ba, sa ) # This will retain the best sa window
-            
-            best = max( best, ba+sb) # Next, we just have to find the largest sb with that best sa
+    def maxSumTwoNoOverlap(self, nums: List[int], firstLen: int, secondLen: int) -> int:
+        prefix = [0]
+        for num in nums:
+            prefix.append(prefix[-1] + num)
 
-        return best
-    
-    def maxSumTwoNoOverlap(self, A: List[int], L: int, M: int) -> int:
-        max_LM_sum = self.trail(A,L,M) # when L is first, followed by M
-        max_ML_sum = self.trail(A,M,L) # when M is first, followed by L
-        return max(max_LM_sum, max_ML_sum)
+        def helper(prefix_sums, firstLen, secondLen):
+            best = 0
+            current = 0
+            for i in range(firstLen + secondLen, len(prefix_sums)):
+                current = max(current, prefix_sums[i - firstLen] - prefix_sums[i - firstLen - secondLen])
+                best = max(best, current + prefix_sums[i] - prefix_sums[i - firstLen])
+
+            return best
+
+        return max(helper(prefix, firstLen, secondLen), helper(prefix, secondLen, firstLen))
