b32e5129b9478a6d23a290458408a9723f60265d
Abdujabbar Mirkhalikov <abdujabbar.mirkhalikov@vendors.cloverhealth.com>
2021-07-03 22:29:15 +0500

    Time: 52 ms (88.15%), Space: 15 MB (44.79%) - LeetHub

---
+++ redundant-connection/redundant-connection.py
@@ -0,0 +1,46 @@
+class UF:
+    def __init__(self, n):
+        self.id = list(range(n + 1))
+        self.size = [1] * n
+        self.count = n
+        
+    def find(self, x):
+        while self.id[x] != x:
+            self.id[x] = self.id[self.id[x]]
+            x = self.id[x]
+        
+        return x
+    
+    def union(self, x, y):
+        x_id = self.find(x)
+        y_id = self.find(y)
+        
+        if x_id == y_id:
+            return
+        
+        if self.size[x_id] < self.size[y_id]:
+            self.id[x_id] = y_id
+            self.size[y_id] += self.size[x_id]
+        else:
+            self.id[y_id] = x_id
+            self.size[x_id] += self.size[y_id]
+        
+        self.count -= 1
+    
+    def is_connected(self, x, y):
+        return self.find(x) == self.find(y)
+            
+
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        uf = UF(1001)
+        
+        
+        for a, b in edges:
+            if uf.is_connected(a, b):
+                return [a, b]
+            else:
+                uf.union(a, b)
+        
+        return []
